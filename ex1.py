901f58572614757aaf49f87ba728144cdbfcbba8
lorenzo00111 <lorenzo02022010@gmail.com>
2026-09-21 09:42:07 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,15 @@
+def calcularMedia(notas):
+    soma=0
+    for nota in notas:
+        soma += nota #soma+soma+nota
+    return soma / len(notas)
+
+notas = [8,9,6]
+media = calcularMedia(notas)
+
+if(media >=7):
+    print("aprovado")
+elif(media >= 5):
+    print("recuperação")
+else:
+    print("reprovado")
